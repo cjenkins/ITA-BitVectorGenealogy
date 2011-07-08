2ae533cf253ea0bee4fd3c5fe82020df9403b1e0
Clinton Jenkins <cjenkins@robowarp.com>
2011-07-08 14:01:35 -0400

    Initial commit.

---
+++ test/BitVectorGenealogy/test/core.clj
@@ -0,0 +1,6 @@
+(ns BitVectorGenealogy.test.core
+  (:use [BitVectorGenealogy.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
